d6e64284dc048c13e5b008715f1802d36aea072c
Лисенко Софія <146013576+Yosohanomi@users.noreply.github.com>
2024-07-31 17:05:05 +0300

    Add files via upload

--- main_window.py
+++ main_window.py
@@ -41,3 +41,42 @@ rb_h.addLayout(rb_v1)
 rb_h.addLayout(rb_v2)
 
 gbAns.setLayout(rb_h)
+
+gb_resuslt = GroupBox()
+v1 = QVBoxLayout()
+v1.addWidget(lb_result)
+v1.addWidget(lb_rightAns)
+gb_result.setLayout(v1)
+
+h1_main = QHBoxLayout()
+h2_main = QHBoxLayout()
+h3_main = QHBoxLayout()
+h4_main = QHBoxLayout()
+
+h1_main.addWidget(btn_menu)
+h1_main.addStretch(1)
+h1_main.addWidget(btn_rest)
+h1_main.addWidget(sp_rest)
+h1_main.addWidget(lb_rest)
+
+h2_main.Widget(lb_question, aligment=(Qt.AlignHCenter | Qt.AlignVCenter))
+
+h3_main.addWidget(gbAns)
+h3_main.addWidget(gb_resuslt)
+gb_resuslt.hide()
+
+h4_main.addStretch(1)
+h4_main.addWidget(btn_next)
+h4_main.addStretch(1)
+
+v1_main = QVBoxLayout()
+v1_main.addLayout(h1_main, stretch=1)
+v1_main.addLayout(h2_main, stretch=2)
+v1_main.addLayout(h3_main, stretch=8)
+v1_main.addLayout(h4_main)
+v1_main.setSpacing(5)
+
+window.setLayout(v1_main)
+window.resize(550, 450)
+
+
