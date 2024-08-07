44f23a9db8942250f6e29ca15dad64749949ce78
Лисенко Софія <146013576+Yosohanomi@users.noreply.github.com>
2024-08-07 16:47:24 +0300

    Update main_window.py

--- main_window.py
+++ main_window.py
@@ -4,9 +4,9 @@ from PyQt5.QtCore import Qt
 
 window = QWidget()
 
-btn_menu = QPushButton()
-btn_rest = QPushButton()
-btn_next = QPushButton()
+btn_menu = QPushButton("Меню")
+btn_rest = QPushButton('Відпочити')
+btn_next = QPushButton('Відповісти')
 
 rb_ans1 = QRadioButton("1")
 rb_ans2 = QRadioButton("2")
@@ -34,19 +34,19 @@ rb_h = QHBoxLayout()
 
 rb_v1.addWidget(rb_ans1)
 rb_v1.addWidget(rb_ans2)
-rb_v2.addWidget(rb_ans3)
-rb_v2.addWidget(rb_ans4)
+rb_v1.addWidget(rb_ans3)
+rb_v1.addWidget(rb_ans4)
 
 rb_h.addLayout(rb_v1)
 rb_h.addLayout(rb_v2)
 
 gbAns.setLayout(rb_h)
 
-gb_result = QGroupBox()
+gb_resuslt = QGroupBox()
 v1 = QVBoxLayout()
 v1.addWidget(lb_result)
 v1.addWidget(lb_rightAns)
-gb_result.setLayout(v1)
+gb_resuslt.setLayout(v1)
 
 h1_main = QHBoxLayout()
 h2_main = QHBoxLayout()
@@ -59,11 +59,11 @@ h1_main.addWidget(btn_rest)
 h1_main.addWidget(sp_rest)
 h1_main.addWidget(lb_rest)
 
-h2_main.addWidget(lb_question, alignment=(Qt.AlignHCenter | Qt.AlignVCenter))
+h2_main.Widget(lb_question, aligment=(Qt.AlignHCenter | Qt.AlignVCenter))
 
 h3_main.addWidget(gbAns)
-h3_main.addWidget(gb_result)
-gb_result.hide()
+h3_main.addWidget(gb_resuslt)
+gb_resuslt.hide()
 
 h4_main.addStretch(1)
 h4_main.addWidget(btn_next)
